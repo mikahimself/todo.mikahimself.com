ffb7e9ef295e961759ede7da3a7732893a3aa71c
mikahimself <mika.laihanen@gmail.com>
2021-05-24 23:28:22 +0300

    Remove config file

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "your-info-here",
-    authDomain: "your-info-here",
-    projectId: "your-info-here",
-    storageBucket: "your-info-here",
-    messagingSenderId: "your-info-here",
-    appId: "your-info-here",
-    measurementId: "your-info-here"
-};
